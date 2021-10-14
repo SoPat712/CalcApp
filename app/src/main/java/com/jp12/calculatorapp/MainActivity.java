aa4bee077207d7f1c69ac9558c381410b383924c
joshpatra12@gmail.com <joshpatra12@gmail.com>
2021-10-13 23:26:16 -0400

    animations and stuff

--- app/src/main/java/com/jp12/calculatorapp/MainActivity.java
+++ app/src/main/java/com/jp12/calculatorapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.jp12.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
@@ -18,23 +19,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
         final LinearLayout linearLayout = findViewById(R.id.linlayout1);
-        final ViewGroup.LayoutParams params = linearLayout.getLayoutParams();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String TAG = "Tag 1";
-                for(int i = 400; i >= 300; i--){
-                    int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, i, getResources().getDisplayMetrics());
-                    params.height = height;
-                    Log.d(TAG, String.valueOf(height));
-                    linearLayout.setLayoutParams(params);
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+
+                if(linearLayout.getLayoutParams().height == 1275){
+                    System.out.println(linearLayout.getLayoutParams().height);
+                    ResizeAnimation resizeAnimation = new ResizeAnimation(
+                            linearLayout,
+                            -150,
+                            1275
+                    );
+                    resizeAnimation.setDuration(150);
+                    view.startAnimation(resizeAnimation);
+                } else{
+                    System.out.println(linearLayout.getLayoutParams().height);
+                    ResizeAnimation resizeAnimation = new ResizeAnimation(
+                            linearLayout,
+                            150,
+                            1125
+                    );
+                    resizeAnimation.setDuration(250);
+                    view.startAnimation(resizeAnimation);
                 }
-                Log.d(TAG, "onClick: asdf");
+
             }
         });
     }
