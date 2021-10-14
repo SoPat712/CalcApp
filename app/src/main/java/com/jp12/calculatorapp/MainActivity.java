1f8616cd55262b53c33abb4215c74e1d97b8ae4f
Josh Patra <joshpatra12@gmail.com>
2021-10-14 14:23:20 -0400

    more ui fixes and other stuff

--- app/src/main/java/com/jp12/calculatorapp/MainActivity.java
+++ app/src/main/java/com/jp12/calculatorapp/MainActivity.java
@@ -3,9 +3,14 @@ package com.jp12.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ValueAnimator;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -17,33 +22,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
+    }
+    public void onClick(View view){
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(500);
+        }
+        System.out.println("vibrated");
+    }
+    public void resizeClick(View view){
         final LinearLayout linearLayout = findViewById(R.id.linlayout1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(linearLayout.getLayoutParams().height == 1275){
-                    System.out.println(linearLayout.getLayoutParams().height);
-                    ResizeAnimation resizeAnimation = new ResizeAnimation(
-                            linearLayout,
-                            -150,
-                            1275
-                    );
-                    resizeAnimation.setDuration(150);
-                    view.startAnimation(resizeAnimation);
-                } else{
-                    System.out.println(linearLayout.getLayoutParams().height);
-                    ResizeAnimation resizeAnimation = new ResizeAnimation(
-                            linearLayout,
-                            150,
-                            1125
-                    );
-                    resizeAnimation.setDuration(250);
-                    view.startAnimation(resizeAnimation);
-                }
-
-            }
-        });
+        getWindow().getDecorView().performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY, HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING);
+        if(linearLayout.getLayoutParams().height == 1275){
+            System.out.println(linearLayout.getLayoutParams().height);
+            ResizeAnimation resizeAnimation = new ResizeAnimation(
+                    linearLayout,
+                    -150,
+                    1275
+            );
+            resizeAnimation.setDuration(150);
+            view.startAnimation(resizeAnimation);
+        } else{
+            System.out.println(linearLayout.getLayoutParams().height);
+            ResizeAnimation resizeAnimation = new ResizeAnimation(
+                    linearLayout,
+                    150,
+                    1125
+            );
+            resizeAnimation.setDuration(250);
+            view.startAnimation(resizeAnimation);
+        }
     }
 }
